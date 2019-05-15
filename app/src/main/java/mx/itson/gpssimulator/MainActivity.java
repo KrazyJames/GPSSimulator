a5a4358ec31a66884eb60a29a7cf6d01fa753505
Jaime Escobar <jaime.escobar.mtz@gmail.com>
2019-05-15 00:03:00 -0700

    POST request issue was solved

--- app/src/main/java/mx/itson/gpssimulator/MainActivity.java
+++ app/src/main/java/mx/itson/gpssimulator/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -14,16 +16,43 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.gson.Gson;
 
+import java.util.List;
+
+import mx.itson.gpssimulator.WebService.Interfaces.LocationJsonAPI;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
     FusedLocationProviderClient fusedLocationClient;
+    Retrofit retrofit;
+    TextView txtVista;
+    Button btnGet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.0.17/potrobus/public/location/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+
+        txtVista = findViewById(R.id.txtVista);
+        btnGet = findViewById(R.id.btnGet);
+
+        btnGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLocations();
+            }
+        });
+
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         findViewById(R.id.btnSend).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,12 +65,59 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onSuccess(Location location) {
                         if (location != null) {
-                            new UbicacionWS().update(location, getApplicationContext());
-                            System.out.println("Message sent");
+                            System.out.println("Location gotten");
+                            insertLocation(location.getLatitude(), location.getLongitude());
+                            System.out.println("Message sent, waiting for response from server...");
                             Toast.makeText(getApplicationContext(), location.getLatitude() + ", " + location.getLongitude(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
+            }
+        });
+    }
+
+    private void getLocations(){
+        LocationJsonAPI locationJsonAPI = retrofit.create(LocationJsonAPI.class);
+        Call<List<mx.itson.gpssimulator.Entidades.Location>> call = locationJsonAPI.getLocations();
+        call.enqueue(new Callback<List<mx.itson.gpssimulator.Entidades.Location>>() {
+            @Override
+            public void onResponse(Call<List<mx.itson.gpssimulator.Entidades.Location>> call, Response<List<mx.itson.gpssimulator.Entidades.Location>> response) {
+                if(!response.isSuccessful()){
+                    txtVista.setText("code: "+response.code());
+                    return;
+                }
+                txtVista.setText("");
+                List<mx.itson.gpssimulator.Entidades.Location> locations = response.body();
+                for(mx.itson.gpssimulator.Entidades.Location l : locations){
+                    String content = new Gson().toJson(l);
+                    txtVista.append(content+"\n");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<mx.itson.gpssimulator.Entidades.Location>> call, Throwable t) {
+                txtVista.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void insertLocation(double lat, double lng){
+        mx.itson.gpssimulator.Entidades.Location lastLocation = new mx.itson.gpssimulator.Entidades.Location(lat, lng);
+        LocationJsonAPI locationJsonAPI = retrofit.create(LocationJsonAPI.class);
+        Call<mx.itson.gpssimulator.Entidades.Location> call = locationJsonAPI.postLocation(lastLocation);
+        call.enqueue(new Callback<mx.itson.gpssimulator.Entidades.Location>() {
+            @Override
+            public void onResponse(Call<mx.itson.gpssimulator.Entidades.Location> call, Response<mx.itson.gpssimulator.Entidades.Location> response) {
+                if (!response.isSuccessful()){
+                    System.out.println("Code"+response.code());
+                    return;
+                }
+                System.out.println("New item added to server: "+response.body().getId());
+            }
+
+            @Override
+            public void onFailure(Call<mx.itson.gpssimulator.Entidades.Location> call, Throwable t) {
+                System.out.println("Failed: " + t.getMessage());
             }
         });
     }
--- app/src/main/java/mx/itson/gpssimulator/UbicacionWS.java
+++ app/src/main/java/mx/itson/gpssimulator/UbicacionWS.java
@@ -6,13 +6,14 @@ import android.location.Location;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Arrays;
+import java.nio.charset.StandardCharsets;
 
 public class UbicacionWS {
 
@@ -25,20 +26,36 @@ public class UbicacionWS {
 
     public void update(final Location location, final Context context)  {
         requestQueue = Volley.newRequestQueue(context);
-        JSONObject jsonObject = new JSONObject();
+        final JSONObject jsonObject = new JSONObject();
         try {
             jsonObject.put("lat", location.getLatitude());
             jsonObject.put("lng", location.getLongitude());
-            System.out.println("Data sending : " + jsonObject.toString());
-            JsonObjectRequest putReq = new JsonObjectRequest(Request.Method.PUT, URL, jsonObject, new Response.Listener<JSONObject>() {
+            System.out.println("Data to send : " + jsonObject.toString());
+            StringRequest postReq = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
                 @Override
-                public void onResponse(JSONObject response) {
-                    System.out.println(response.toString());
+                public void onResponse(String response) {
+                    System.out.println("Response from server:");
+                    System.out.println(response);
                 }
-            }, null);
-            System.out.println("Content-Type: "+putReq.getBodyContentType());
-            System.out.println("Body: "+ new String(putReq.getBody()));
-            requestQueue.add(putReq);
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    System.out.println("Error while POST:");
+                    error.printStackTrace();
+                }
+            }){
+                @Override
+                public String getBodyContentType() {
+                    return "Accept: application/json";
+                }
+
+                @Override
+                public byte[] getBody() {
+                    return jsonObject.toString().getBytes(StandardCharsets.UTF_8);
+                }
+            };
+            System.out.println(postReq.getBodyContentType());
+            requestQueue.add(postReq);
         } catch (JSONException e) {
             e.printStackTrace();
         }
